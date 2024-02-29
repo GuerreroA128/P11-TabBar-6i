563e6ae172c635349cfe6f6fd03c7672c2e72d33
GuerreroA128 <143743819+GuerreroA128@users.noreply.github.com>
2024-02-29 12:04:31 -0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,84 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppMiTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppMiTabBar extends StatelessWidget {
+  const AppMiTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Ejemplo TabBar Angel Guerrero",
+        theme: ThemeData(primarySwatch: Colors.cyan),
+        home: MiPaginaInicial());
   }
-}
+} //fin AppMiTabBar
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+} //MiPaginaInicial
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("TabBar Angel Guerrero"),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: "INICIO",
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                text: "CUENTA",
+                icon: Icon(Icons.verified_user),
+              ),
+              Tab(
+                text: "VENTA",
+                icon: Icon(Icons.shopping_bag),
+              ),
+              Tab(
+                text: "AJUSTES",
+                icon: Icon(Icons.settings),
+              ),
+            ], //fin de tabs
+          ), //fin buttontabb
         ),
+        body: TabBarView(children: const <Widget>[
+          Center(
+            child: Text(
+              "Chivas BEE BEE CHIVAS:)",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Tu Cuenta",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Compra",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Tu Configuracion",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+        ]), //JARIN DE NINOS
       ),
     );
-  }
-}
+  } //fin widgets
+} //_MiPaginaInicialState
